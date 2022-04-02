3f10312c88458a45aedb5cd9d510a8c4de95ed97
Elizabeth Christman <elizabethc99@vt.edu>
2022-04-02 14:07:41 -0400

    completed draft of line chart

--- js/lineChart.js
+++ js/lineChart.js
@@ -11,18 +11,25 @@ class LineChart{
     initVis(){
         let vis = this;
 
-        vis.margin = {top: 10, right: 50, bottom: 10, left: 50};
+        vis.margin = {top: 20, right: 20, bottom: 20, left: 50};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
-            .attr("width", vis.width)
-            .attr("height", vis.height)
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
             .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
-        vis.x = d3.scaleLinear().range([0, vis.width]);
-        vis.y = d3.scaleLinear().range([vis.height, 0]);
+        vis.x = d3.scaleTime()
+            .domain(d3.extent(vis.data, function(d) { return d.chart_date; }))
+            .range([vis.margin.left, vis.width - vis.margin.left]);
+
+
+        vis.y = d3.scaleLinear()
+            .domain([0, d3.max(vis.data, function(d) { return d.n; })])
+            .range([vis.height, vis.margin.bottom]);
+
 
         vis.xAxis =  vis.svg.append("g")
             .attr("class", "x-axis axis")
@@ -31,7 +38,12 @@ class LineChart{
 
         vis.yAxis = vis.svg.append("g")
             .attr("class", "y-axis axis")
+            .attr("transform", "translate(" + vis.margin.left + ", 0)")
             .call(d3.axisLeft(vis.y));
+
+        vis.tooltip = d3.select("body").append('div')
+            .attr('class', "tooltip")
+            .attr('id', 'lineTooltip')
 
 
 
@@ -41,27 +53,22 @@ class LineChart{
     wrangleData(){
         let vis = this;
 
-        vis.data.forEach(album => {
-            vis.displayData.push({
-                year: album.chart_date.getFullYear(),
-                danceability: album.danceability
+        console.log(vis.data);
 
+        vis.attributes = ["acousticness","danceability","energy","instrumentalness","liveness","speechiness","valence"];
+
+        for(let i=0; i<vis.attributes.length; i++) {
+            let array = [];
+            vis.data.forEach(row => {
+                array.push({
+                    year: row.chart_date,
+                    value: row[vis.attributes[i]]
+                })
             });
-        });
+            vis.displayData.push(array);
+        }
 
-        //only computes average for dancability
-        vis.displayData = vis.displayData.reduce(function(h, obj) {
-            h[obj.year] = (h[obj.year] || []).concat(obj);
-            return h;
-        }, {});
-
-        vis.displayData = Object.keys(vis.displayData).map(key => {
-            return {
-                year: key,
-                danceabilityAvg : vis.displayData[key].reduce((a, b) => a + (b.danceability || 0), 0)/vis.displayData[key].length,
-            }
-        });
-
+        console.log(vis.displayData)
 
         vis.updateVis()
     }
@@ -69,38 +76,50 @@ class LineChart{
     updateVis(){
         let vis = this;
 
-        vis.x.domain([d3.min(vis.displayData, function(d) {return d.year;}),
-                    d3.max(vis.displayData, function(d) {return d.year;})]);
+        vis.x.domain(d3.extent(vis.displayData[0], function(d) { return d.year; }));
 
-        vis.y.domain([0,
-                        d3.max(vis.displayData, function(d) {return d.danceabilityAvg;})]);
+        vis.y.domain([0, 1]);
 
         vis.svg.select(".x-axis").call(d3.axisBottom(vis.x));
         vis.svg.select(".y-axis").call(d3.axisLeft(vis.y));
 
-        // let lineChart = vis.svg.selectAll(".line")
-        //     .data(vis.displayData);
-        //
-        // lineChart.enter()
-        //     .append('path')
-        //     .attr("class", "line")
-        //     .attr("stroke-width", 1.5)
-        //     .attr("d", d3.line()
-        //         .x(function(d) { return vis.x(d.year) })
-        //         .y(function(d) { return vis.y(d.danceabilityAvg) })
-        //     );
-        //lineChart.exit().remove();
 
+        vis.colors = ['#e41a1c','#ff7f00', '#377eb8','#4daf4a','#984ea3', '#030300','#ffff33']
+        for(let i = 0; i < vis.displayData.length; i++){
+            vis.svg.append("path")
+                .datum(vis.displayData[i])
+                .attr("fill", "none")
+                .attr("stroke", vis.colors[i])
+                .attr("stroke-width", "2px")
+                .attr("d", d3.line()
+                    .x(function(d) { return vis.x(d.year) })
+                    .y(function(d) { return vis.y(d.value) })
+                )
+                .on('mouseover', function(event){
+                    d3.select(this)
+                        .attr('stroke-width', '4px')
+                    vis.tooltip
+                        .style("opacity", 1)
+                        .style("left", event.pageX + 20 + "px")
+                        .style("top", event.pageY + "px")
+                        .html(`
+                         <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 5px">
+                             <h4 style="font-size: 14px"> Attribute: ${vis.attributes[i]}</h4>
 
-        vis.svg.append("path")
-            .datum(vis.displayData)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(function(d) { return vis.x(d.year) })
-                .y(function(d) { return vis.y(d.danceabilityAvg) })
-            )
+                         </div>`
+                        );
+                })
+                .on('mouseout', function(){
+                    d3.select(this)
+                        .attr('stroke', vis.colors[i])
+                        .attr('stroke-width', '2px')
+                    vis.tooltip
+                        .style("opacity", 0)
+                        .style("left", 0)
+                        .style("top", 0)
+                        .html(``);
+                })
+        }
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let introLineChart;
 
-let parseDate = d3.timeParse("%Y-%m-%d");
+let parseDate = d3.timeParse("%Y");
 
 
 
@@ -8,7 +8,31 @@ let parseDate = d3.timeParse("%Y-%m-%d");
 loadData();
 
 function loadData() {
-    d3.csv("data/album_data.csv").then(csv=> {
+    // d3.csv("data/album_data.csv").then(csv=> {
+    //
+    //     csv.forEach(function(d){
+    //         d.acousticness = +d.acousticness;
+    //         d.danceability = +d.danceability;
+    //         d.duration_ms = +d.duration_ms;
+    //         d.energy = +d.energy;
+    //         d.instrumentalness = +d.instrumentalness;
+    //         d.liveness = +d.liveness;
+    //         d.loudness = +d.loudness;
+    //         d.energy = +d.energy;
+    //         d.mode = +d.mode;
+    //         d.rank = +d.rank;
+    //         d.speechiness = +d.speechiness;
+    //         d.tempo = +d.tempo;
+    //         d.time_signature = +d.time_signature;
+    //         d.valence = +d.valence;
+    //         d.key = +d.key;
+    //
+    //         d.chart_date = parseDate(d.chart_date);
+    //         d.release_date = parseDate(d.release_date);
+    //
+    //     });
+
+    d3.csv("data/yearly_avgs.csv").then(csv=> {
 
         csv.forEach(function(d){
             d.acousticness = +d.acousticness;
@@ -19,16 +43,12 @@ function loadData() {
             d.liveness = +d.liveness;
             d.loudness = +d.loudness;
             d.energy = +d.energy;
-            d.mode = +d.mode;
-            d.rank = +d.rank;
             d.speechiness = +d.speechiness;
             d.tempo = +d.tempo;
             d.time_signature = +d.time_signature;
             d.valence = +d.valence;
-            d.key = +d.key;
 
             d.chart_date = parseDate(d.chart_date);
-            d.release_date = parseDate(d.release_date);
 
         });
 
