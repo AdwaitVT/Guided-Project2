1762e99d7ba56b8df3111e343d337d08e58a8c50
Elizabeth Christman <elizabethc99@vt.edu>
2022-05-04 11:31:08 -0400

    updating styling

--- js/sliders-temp.js
+++ js/sliders-temp.js
@@ -57,7 +57,7 @@ class Sliders{
             .attr('width', 500)
             .attr('height', 100)
             .append('g')
-            .attr('transform', 'translate(30,60)');
+            .attr('transform', 'translate(30,30)');
 
         vis.sliderRange2 = d3.sliderBottom()
             .min(0.0)
@@ -75,7 +75,7 @@ class Sliders{
             .min(0.0)
             .max(1.0)
             .width(300)
-            .tickFormat(d3.format('.2%'))
+            .tickFormat(d3.format('.2'))
             .ticks(10)
             .default([0.0, 1.0])
             .fill('#2196f3')
@@ -86,17 +86,47 @@ class Sliders{
             .attr('width', 500)
             .attr('height', 100)
             .append('g')
-            .attr('transform', 'translate(30,90)');
+            .attr('transform', 'translate(30,30)');
 
         vis.attribute3Values = [0,1];
 
+        vis.columns = ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s']
+        vis.years = [
+            {'1960s': "", '1970s': 1970, '1980s': 1980, '1990s': 1990, '2000s': 2000, '2010s': 2010},
+            {'1960s': "", '1970s': 1971, '1980s': 1981, '1990s': 1991, '2000s': 2001, '2010s': 2011},
+            {'1960s': "", '1970s': 1972, '1980s': 1982, '1990s': 1992, '2000s': 2002, '2010s': 2012},
+            {'1960s': 1963, '1970s': 1973, '1980s': 1983, '1990s': 1993, '2000s': 2003, '2010s': 2013},
+            {'1960s': 1964, '1970s': 1974, '1980s': 1984, '1990s': 1994, '2000s': 2004, '2010s': 2014},
+            {'1960s': 1965, '1970s': 1975, '1980s': 1985, '1990s': 1995, '2000s': 2005, '2010s': 2015},
+            {'1960s': 1966, '1970s': 1976, '1980s': 1986, '1990s': 1996, '2000s': 2006, '2010s': 2016},
+            {'1960s': 1967, '1970s': 1977, '1980s': 1987, '1990s': 1997, '2000s': 2007, '2010s': 2017},
+            {'1960s': 1968, '1970s': 1978, '1980s': 1988, '1990s': 1998, '2000s': 2008, '2010s': 2018},
+            {'1960s': 1969, '1970s': 1979, '1980s': 1989, '1990s': 1999, '2000s': 2009, '2010s': 2019}
+
+        ]
 
 
-        // vis.svg.append("text")
-        //     .attr("x", vis.width/2)
-        //     .attr("y", vis.height/2)
-        //     .attr("font-size", "14px")
-        //     .text("The year that matches the given values is: ");
+
+        vis.table = d3.select('#year-table').append('table');
+        vis.thead = vis.table.append('thead');
+        vis.tbody = vis.table.append('tbody');
+
+        vis.rows = vis.tbody.selectAll('tr')
+            .data(vis.years)
+            .enter()
+            .append('tr');
+
+        vis.cells = vis.rows.selectAll('td')
+            .data(function (row) {
+                return vis.columns.map(function (column) {
+                    return {column: column, value: row[column]};
+                });
+            })
+            .enter()
+            .append('td')
+            .attr("id", function(d) {return d.value})
+            .text(function (d) { return d.value})
+
 
         this.wrangleData();
 
@@ -156,7 +186,7 @@ class Sliders{
             if(row[vis.attribute1] >= vis.attribute1Values[0] && row[vis.attribute1] <= vis.attribute1Values[1]){
                 if(row[vis.attribute2] >= vis.attribute2Values[0] && row[vis.attribute2] <= vis.attribute2Values[1]){
                     if(row[vis.attribute3] >= vis.attribute3Values[0] && row[vis.attribute3] <= vis.attribute3Values[1]){
-                        vis.displayData.push(row);
+                        vis.displayData.push(vis.formatTime(row.chart_date));
                     }
                 }
             }
@@ -165,10 +195,7 @@ class Sliders{
 
 
         console.log(vis.displayData);
-        // console.log(vis.formatTime(vis.displayData[0].chart_date));
-        if(vis.displayData.length != 0){
-            vis.year = vis.formatTime(vis.displayData[0].chart_date);
-        }
+
 
         this.updateVis();
 
@@ -179,6 +206,9 @@ class Sliders{
         let vis = this;
 
         vis.svg.selectAll(".year").remove();
+        d3.selectAll("td").each(function() {
+            d3.select(this).style("color", "lightgrey");
+        });
 
         d3.select('p#value-range1')
             .text(vis.sliderRange1
@@ -209,7 +239,19 @@ class Sliders{
             .attr("x", vis.width/2)
             .attr("y", vis.height/2)
             .attr("font-size", "14px")
-            .text(`The year that matches the given values is: ${vis.year}`);
+            .text(`The year that matches the given values is: ${vis.displayData[0]}`);
+
+        d3.selectAll("td").each(function() {
+            vis.displayData.forEach(y => {
+                if(d3.select(this).text() == y){
+                    //console.log(this.text());
+                    d3.select(this).style("color", "black");
+                }
+                //console.log(d3.select(this).text());
+            })
+        })
+
+
 
         vis.svg.exit().remove();
 
